1bcd6a58d1d4df155cbcd1bf11d12279762d98e3
hamonangann <70834922+hamonangann@users.noreply.github.com>
2023-12-05 02:28:37 +0700

    tidy up frontend

--- web/views.py
+++ web/views.py
@@ -1,15 +1,22 @@
 from django.shortcuts import render
 from retriever.search import retrieve
+import time
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
 
 def list_docs(request):
+    time_start = time.time()
+
     query = request.GET.get('query')
-    return render(request, 'list_docs.html', {'docs': retrieve(query)})
+    docs = retrieve(query)
+
+    time_stop = time.time()
+    time_count = time_stop - time_start
+
+    return render(request, 'list_docs.html', {'query': query, 'docs': docs, 'time_count': time_count})
 
 def view_doc(request, doc_id):
-    # doc = Doc.objects.get(id=doc_id)
     with open('retriever/collections/0/' + doc_id, 'r') as f:
-        return render(request, 'view_doc.html', {'doc': f.read()})
+        return render(request, 'view_doc.html', {'doc_name': doc_id, 'doc_content': f.read()})
