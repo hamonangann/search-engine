89c5474633eaa60d72e9ae716cd28919c2259509
hamonangann <70834922+hamonangann@users.noreply.github.com>
2023-12-05 02:41:10 +0700

    fix static deploy

--- search/settings.py
+++ search/settings.py
@@ -155,7 +155,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'web/static/'
-STATIC_ROOT = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
